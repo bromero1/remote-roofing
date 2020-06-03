642b74fad93a99fe635664ab2c6c3e40bef6f783
bromero1 <bromero52@gmail.com>
2020-06-03 14:15:40 -0400

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-//constructor(database: string, username: string, password: string, options: object)
+// Password in file for simplicity 
 module.exports =  new Sequelize('remoteroofing', 'bryan', 'remoteroofing', {
   host: 'remoteroofing-test.c8vl47uqm7vj.us-east-2.rds.amazonaws.com',
   dialect: 'postgres',
